c6caf18950a4fcd2b191b61965356799378e4209
Aditya Kumar <42202518+imadityagolu@users.noreply.github.com>
2025-02-24 20:56:58 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let mydiv = document.getElementById("div");
+mydiv.style.backgroundColor = "yellow";
+mydiv.style.margin = "20px";
+mydiv.style.padding = "10px"; 
+mydiv.style.fontSize = "18px"; 
+mydiv.style.fontWeight = "bold"; 
+mydiv.style.height = "200px"; 
+mydiv.style.width = "300px";
